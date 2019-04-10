40252e77b4f4edfaba16ef9d03d31bf07411e2de
chieveit <javachieveit@163.com>
2019-04-10 22:34:44 +0800

    add missing  @babel/runtime

--- bin/create.js
+++ bin/create.js
@@ -2,9 +2,19 @@ module.exports = function(program) {
   const path = require('path');
   const fs = require('fs-extra');
   const log = require('./log');
+  const { version } = require('../package');
 
   program.command('create <name>').action(function(name) {
     fs.copySync(path.resolve(__dirname, '../example'), path.resolve(name));
+
+    let packageJSON = fs.readFileSync(
+      path.resolve(name, './package.json'),
+      'utf8',
+    );
+    packageJSON = packageJSON.replace(`project-name`, name);
+    packageJSON = packageJSON.replace(`min-version`, version);
+    fs.writeFileSync(path.resolve(name, './package.json'), packageJSON);
+
     log.info(`create project: ${name}`);
   });
 };
