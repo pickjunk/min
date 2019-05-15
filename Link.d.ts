a0fb43949706849b0b5ce42709714042659485bc
chieveit <javachieveit@163.com>
2019-05-15 17:29:48 +0800

    fix Link

--- Link.d.ts
+++ Link.d.ts
@@ -3,7 +3,7 @@ import { Params } from './routes';
 interface Props {
     to: string;
     args?: Params;
-    children: ReactElement;
+    children: ReactElement | string;
 }
 export default function Link({ to, args, children, ...props }: Props): JSX.Element;
 export {};
