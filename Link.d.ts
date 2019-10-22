d08de220dfce72e91eeb0198904560b58c4ce96d
ChieveiT <javachieveit@163.com>
2019-10-22 15:00:42 +0800

    package.json version

--- Link.d.ts
+++
@@ -1,9 +0,0 @@
-import { ReactElement } from 'react';
-import { Params } from './routes';
-interface Props {
-    to: string;
-    args?: Params;
-    children: ReactElement | string;
-}
-export default function Link({ to, args, children, ...props }: Props): JSX.Element;
-export {};
