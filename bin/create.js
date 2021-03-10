d0aab230e0b857cf99372f6051bb6282e1b4beb8
chieveit <javachieveit@163.com>
2021-03-10 18:18:48 +0000

    分离出min-template项目，完成create指令

--- bin/create.js
+++ bin/create.js
@@ -9,7 +9,7 @@ module.exports = function (program) {
 
   // Thanks to https://futurestud.io/tutorials/axios-download-progress-in-node-js
   async function download(url, dest) {
-    log.info('Connecting …');
+    log.info('Connecting...');
     const { data, headers } = await axios({
       url,
       timeout: 60 * 1000,
@@ -18,13 +18,13 @@ module.exports = function (program) {
     });
     const totalLength = headers['content-length'];
 
+    log.info('Downloading...');
+
     const bar = new cliProgress.SingleBar(
       {},
       cliProgress.Presets.shades_classic,
     );
     bar.start(totalLength, 0);
-
-    log.info('Starting download');
 
     const writer = fs.createWriteStream(path.resolve(__dirname, dest));
 
@@ -42,33 +42,49 @@ module.exports = function (program) {
     });
   }
 
-  program.command('create <name>').action(async function (name) {
-    const tmpDir = path.resolve(__dirname, '../tmp');
-    const zip = path.join(tmpDir, 'template.zip');
-    const unzip = path.join(tmpDir, 'min-template-main');
+  program
+    .command('create <template> <project>')
+    .action(async function (template, project) {
+      log.info(`create project [${project}] with template [${template}]`);
 
-    if (!fs.existsSync(tmpDir)) {
-      fs.mkdirSync(tmpDir);
-    }
-    await download(
-      'https://github.com/pickjunk/min-template/archive/main.zip',
-      zip,
-    );
-    await fs
-      .createReadStream(zip)
-      .pipe(unzipper.Extract({ path: tmpDir }))
-      .promise();
+      const tmpDir = path.resolve(__dirname, '../template');
+      const zip = path.join(tmpDir, 'template.zip');
 
-    fs.copySync(unzip, path.resolve(name));
+      if (!fs.existsSync(tmpDir)) {
+        fs.mkdirSync(tmpDir);
+      }
+      await download(
+        'https://api.github.com/repos/pickjunk/min-template/zipball/main',
+        zip,
+      );
+      await fs
+        .createReadStream(zip)
+        .pipe(unzipper.Extract({ path: tmpDir }))
+        .promise();
 
-    // let packageJSON = fs.readFileSync(
-    //   path.resolve(name, './package.json'),
-    //   'utf8',
-    // );
-    // packageJSON = packageJSON.replace(`project-name`, name);
-    // // packageJSON = packageJSON.replace(`min-version`, version);
-    // fs.writeFileSync(path.resolve(name, './package.json'), packageJSON);
+      const entries = fs.readdirSync(tmpDir);
+      let unzip = '';
+      const r = new RegExp('^pickjunk-min-template-.*$');
+      for (let entry of entries) {
+        if (r.test(entry)) {
+          unzip = path.join(tmpDir, entry, template);
+          break;
+        }
+      }
 
-    log.info(`create project: ${name}`);
-  });
+      fs.copySync(unzip, path.resolve(project));
+      fs.rmdirSync(tmpDir, {
+        recursive: true,
+      });
+
+      let packageJSON = fs.readFileSync(
+        path.resolve(project, './package.json'),
+        'utf8',
+      );
+      packageJSON = packageJSON.replace(`min-${template}`, project);
+      // packageJSON = packageJSON.replace(`min-version`, version);
+      fs.writeFileSync(path.resolve(project, './package.json'), packageJSON);
+
+      log.info(`Done.`);
+    });
 };
