4d385eee9e7367a63808fffe306517243e9958c9
chieveit <javachieveit@163.com>
2021-07-21 07:17:38 +0000

    push和replace支持传入context

--- Router.d.ts
+++ Router.d.ts
@@ -1,6 +1,15 @@
 import React from 'react';
 import { Routes, LoadedRoute, Location, Routing, Component } from './routes';
-export interface RouterContext extends LoadedRoute {
+export interface Context {
+    [key: string]: any;
+}
+export interface RouterLocation extends Location {
+    context?: Context;
+}
+export interface RouterLoadedRoute extends LoadedRoute {
+    context?: Context;
+}
+export interface RouterContext extends RouterLoadedRoute {
     routes: Routes;
     loading: boolean;
 }
@@ -9,12 +18,12 @@ export interface ReachHandler {
 }
 declare function createRouter({ routes, initialRoute, likeApp, }: {
     routes: Routes;
-    initialRoute: LoadedRoute;
+    initialRoute: RouterLoadedRoute;
     likeApp: boolean;
 }): Promise<React.FC<{}>>;
 export default createRouter;
-export declare function push(location: Location): void;
-export declare function replace(location: Location): void;
+export declare function push(location: RouterLocation): void;
+export declare function replace(location: RouterLocation): void;
 export declare function back(): void;
 export declare function forward(): void;
 export declare function link(location: Location): string;
